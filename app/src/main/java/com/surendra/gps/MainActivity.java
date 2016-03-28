bc1b944352402f30eab5f8a042efcfe30413744a
surendra <surendrak96@gmail.com>
2016-03-28 19:50:19 +0530

    first commit

---
+++ app/src/main/java/com/surendra/gps/MainActivity.java
@@ -0,0 +1,49 @@
+package com.surendra.gps;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.widget.TextView;
+
+import android.util.Log;
+
+public class MainActivity extends Activity implements LocationListener {
+    protected LocationManager locationManager;
+
+    TextView txtLat;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtLat = (TextView) findViewById(R.id.textview1);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+    }
+    @Override
+    public void onLocationChanged(Location location) {
+        txtLat = (TextView) findViewById(R.id.textview1);
+        txtLat.setText("Latitude  : " + location.getLatitude() + "\n Longitude  : " + location.getLongitude());
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
+    }
+}
